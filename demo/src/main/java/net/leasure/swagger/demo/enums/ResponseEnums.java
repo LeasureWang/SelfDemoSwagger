0c387348c5d0c52465c180d8081e048e656bd33c
Leasure_WANG <wanglixu@sgepri.sgcc.com.cn>
2019-12-08 09:51:30 +0800

    updated swagger project

--- demo/src/main/java/net/leasure/swagger/demo/enums/ResponseEnums.java
+++ demo/src/main/java/net/leasure/swagger/demo/enums/ResponseEnums.java
@@ -3,13 +3,14 @@ package net.leasure.swagger.demo.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public enum ResponseEnums {
 
-    REP_SUCCESS(200, "Success"),
+    REP_SUCCESS(HttpStatus.OK.value(), "Success"),
 
     REP_PARAM_ERROR(5001, "Invalid params"),
     REP_SYSTEM_ERROR(5002, "System error");
